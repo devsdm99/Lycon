79106988c8215f1e925fb0e9d8089ad24f49d988
Sergio Diaz <devsdm99@gmail.com>
2019-12-19 08:38:27 +0100

    - Nuevo Swipe Page, nuevo layout

--- lib/Pages/home_page.dart
+++ lib/Pages/home_page.dart
@@ -1,100 +1,44 @@
-import 'package:dartapp/SizeConfig.dart';
-import 'package:dartapp/models/CardInfo.dart';
+import 'package:dartapp/Pages/chat_page.dart';
+import 'package:dartapp/Pages/swipe_page.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_tindercard/flutter_tindercard.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  final _pageOptions = [
+    SwipePage(),
+    ChatScreen()
+  ];
+
+  int _selectedIndex = 0;
+    void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: new Stack(
-        children: <Widget>[
-          _fondoPantalla(),
-          _cartas(context),
-        ],
-      ),
+      body: _pageOptions[_selectedIndex],
       bottomNavigationBar: _navigationBar(),
     );
   }
 
-  Widget _fondoPantalla() {
-    return Container(
-      height: double.infinity,
-      width: double.infinity,
-      color: Color.fromRGBO(29, 29, 28, 1.0),
-    );
-  }
-
-  Widget _cartas(BuildContext context) {
-    CardController controller;
-    List<String> welcomeImages = [
-      "assets/images/1.jpg",
-      "assets/images/background.jpg",
-      "assets/images/3.jpg",
-      "assets/images/3.jpg",
-      "assets/images/3.jpg",
-      "assets/images/3.jpg",
-      "assets/images/3.jpg",
-    ];
-    List<CardInfo> personas = [
-      new CardInfo("Alexis Ren", "18", "Los Angeles", "assets/images/1.jpg"),
-      new CardInfo("Miriam", "18", "Los Angeles", "assets/images/2.jpg"),
-      new CardInfo("Tania", "18", "Los Angeles", "assets/images/3.jpg"),
-      new CardInfo("Tania", "18", "Los Angeles", "assets/images/3.jpg"),
-      new CardInfo("Tania", "18", "Los Angeles", "assets/images/3.jpg"),
-      new CardInfo("Tania", "18", "Los Angeles", "assets/images/3.jpg"),
-      new CardInfo("Tania", "18", "Los Angeles", "assets/images/3.jpg"),
-    ];
-    //Image.asset('${personas[index].photoURL}', fit: BoxFit.contain,)
-    return Container(
-      height: MediaQuery.of(context).size.height,
-      width: MediaQuery.of(context).size.width,
-      child: new TinderSwapCard(
-        orientation: AmassOrientation.BOTTOM,
-        totalNum: 7,
-        stackNum: 3,
-        swipeEdge: 6.0,
-        maxWidth: MediaQuery.of(context).size.width * 0.9,
-        maxHeight: MediaQuery.of(context).size.width * 0.9,
-        minWidth: MediaQuery.of(context).size.width * 0.8,
-        minHeight: MediaQuery.of(context).size.width * 0.8,
-        cardBuilder: (context, index) =>
-            //CREAR EL MODELO DE CARTA    
-         Container(
-           
-          color: Colors.red,
-          child: Stack(
-            children: <Widget>[ 
-              _datosPersona(personas, index),
-              Image(
-                  image: AssetImage(personas[index].photoURL), 
-                ),
-            ],
-          ),
-        ),
-        cardController: controller = CardController(),
-        swipeUpdateCallback: (DragUpdateDetails details, Alignment align) {
-          /// Get swiping card's alignment
-          if (align.x < 0) {
-            //Card is LEFT swiping
-            print("IZQUIERDA");
-          } else if (align.x > 0) {
-            print("DERECHA");
-          }
-        },
-        swipeCompleteCallback: (CardSwipeOrientation orientation, int index) {
-          /// Get orientation & index of swiped card!
-        },
-      ),
-    );
-  }
-
   Widget _navigationBar() {
+    
     return BottomNavigationBar(
       type: BottomNavigationBarType.fixed,
       backgroundColor: Color.fromRGBO(46, 46, 44, 1.0),
       selectedItemColor: Color.fromRGBO(247, 88, 106, 1.0),
+      onTap: _onItemTapped,
+      currentIndex: _selectedIndex,
       items: [
         BottomNavigationBarItem(
           icon: Icon(
@@ -105,7 +49,7 @@ class HomePage extends StatelessWidget {
         ),
         BottomNavigationBarItem(
           icon: Icon(
-            CupertinoIcons.heart,
+            Icons.chat,
             size: 35,
           ),
           title: Container(),
@@ -128,24 +72,5 @@ class HomePage extends StatelessWidget {
     );
   }
 
-  Widget _datosPersona(List<CardInfo> personas, int index) {
-    return SafeArea(
-      child: Container(
-        padding: EdgeInsets.all(20),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Text(
-              '${personas[index].name}, ${personas[index].age}',
-              style: TextStyle(color: Colors.white, fontSize: 25.0),
-            ),
-            Text(
-              personas[index].country,
-              style: TextStyle(color: Colors.white, fontSize: 25.0),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
+
 }
---
+++ lib/Pages/swipe_page.dart
@@ -0,0 +1,127 @@
+import 'package:dartapp/models/CardInfo.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_tindercard/flutter_tindercard.dart';
+
+class SwipePage extends StatefulWidget {
+  @override
+  _SwipePageState createState() => _SwipePageState();
+}
+
+class _SwipePageState extends State<SwipePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: new Stack(
+        children: <Widget>[
+          _fondoPantalla(),
+          _cartas(context),
+        ],
+      ),
+    );
+  }
+
+  Widget _fondoPantalla() {
+    return Container(
+      height: double.infinity,
+      width: double.infinity,
+      color: Color.fromRGBO(29, 29, 28, 1.0),
+    );
+  }
+
+  Widget _cartas(BuildContext context) {
+    CardController controller;
+    List<String> welcomeImages = [
+      "assets/images/1.jpg",
+      "assets/images/background.jpg",
+      "assets/images/3.jpg",
+      "assets/images/3.jpg",
+      "assets/images/3.jpg",
+      "assets/images/3.jpg",
+      "assets/images/3.jpg",
+    ];
+    List<CardInfo> personas = [
+      new CardInfo(
+          "Alexis Ren", "18", "Los Angeles", "assets/images/1.jpg"),
+      new CardInfo(
+          "Leo", "21", "Vilanova y la Geltrú", "assets/images/4.jpg"),
+      new CardInfo("Mireia", "23", "Madrid", "assets/images/3.jpg"),
+      new CardInfo("Tania", "19", "Valencia", "assets/images/2.jpg"),
+      new CardInfo("Nacho", "18", "Barcelona", "assets/images/5.jpg"),
+      new CardInfo("Claire", "19", "Lyon", "assets/images/2.jpg"),
+    ];
+    //Image.asset('${personas[index].photoURL}', fit: BoxFit.contain,)
+    return Container(
+      child: new TinderSwapCard(
+        orientation: AmassOrientation.BOTTOM,
+        totalNum: 6,
+        stackNum: 3,
+        swipeEdge: 6.0,
+        maxWidth: MediaQuery.of(context).size.width,
+        maxHeight: MediaQuery.of(context).size.height * 0.9,
+        minWidth: MediaQuery.of(context).size.width * 0.8,
+        minHeight: MediaQuery.of(context).size.height * 0.8,
+        cardBuilder: (context, index) => Container(
+          color: Color.fromRGBO(29, 29, 28, 1.0),
+
+          //color: Color.fromRGBO(29, 29, 28, 1.0),
+          child: Stack(
+            fit: StackFit.expand,
+            children: <Widget>[
+              Column(
+                children: <Widget>[
+                  _datosPersona(personas, index),
+                  Expanded(
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(20),
+                      child: Image(
+                        image: AssetImage(personas[index].photoURL),
+                        fit: BoxFit.fill,
+                      ),
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+        cardController: controller = CardController(),
+        swipeUpdateCallback: (DragUpdateDetails details, Alignment align) {
+          /// Get swiping card's alignment
+          if (align.x < 0) {
+            //Card is LEFT swiping
+            print("IZQUIERDA");
+          } else if (align.x > 0) {
+            print("DERECHA");
+          }
+        },
+        swipeCompleteCallback: (CardSwipeOrientation orientation, int index) {
+          /// Get orientation & index of swiped card!
+        },
+      ),
+    );
+  }
+
+  Widget _datosPersona(List<CardInfo> personas, int index) {
+    TextStyle estilo = new TextStyle(fontWeight: FontWeight.bold, color: Colors.white, fontSize: 23.0);
+
+    return SafeArea(
+      child: Container(
+        padding: EdgeInsets.all(20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              '${personas[index].name}, ${personas[index].age}',
+              style: estilo,
+            ),
+            Text(
+              personas[index].country,
+              style: estilo,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
