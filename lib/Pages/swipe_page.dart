d826fb20a64244752771d46bf9fd9951b6a79cf2
Sergio Diaz <devsdm99@gmail.com>
2020-01-21 22:52:04 +0100

    Arreglado el fit de las fotografias

--- lib/Pages/swipe_page.dart
+++ lib/Pages/swipe_page.dart
@@ -16,6 +16,23 @@ class _SwipePageState extends State<SwipePage> {
         children: <Widget>[
           _fondoPantalla(),
           _cartas(context),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(bottom:20.0, left: 20, right: 20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    FloatingActionButton(),
+                    FloatingActionButton(),
+                    FloatingActionButton(),
+                  ],
+                ),
+              )
+            ],
+          )
         ],
       ),
     );
@@ -32,12 +49,9 @@ class _SwipePageState extends State<SwipePage> {
   Widget _cartas(BuildContext context) {
     CardController controller;
 
-    
     List<CardInfo> personas = [
-      new CardInfo(
-          "Alexis Ren", "18", "Los Angeles", "assets/images/1.jpg"),
-      new CardInfo(
-          "Leo", "21", "Vilanova y la Geltrú", "assets/images/4.jpg"),
+      new CardInfo("Alexis Ren", "18", "Los Angeles", "assets/images/1.jpg"),
+      new CardInfo("Leo", "21", "Vilanova y la Geltrú", "assets/images/4.jpg"),
       new CardInfo("Mireia", "23", "Madrid", "assets/images/3.jpg"),
       new CardInfo("Tania", "19", "Valencia", "assets/images/2.jpg"),
       new CardInfo("Nacho", "18", "Barcelona", "assets/images/5.jpg"),
@@ -45,37 +59,36 @@ class _SwipePageState extends State<SwipePage> {
     ];
     //Image.asset('${personas[index].photoURL}', fit: BoxFit.contain,)
     return Container(
+      color: Colors.yellow,
+      width: MediaQuery.of(context).size.width,
+      height: MediaQuery.of(context).size.height * 0.8,
       child: new TinderSwapCard(
         orientation: AmassOrientation.BOTTOM,
-        totalNum: 6,
+        totalNum: personas.length,
         stackNum: 3,
         swipeEdge: 6.0,
         maxWidth: MediaQuery.of(context).size.width,
-        maxHeight: MediaQuery.of(context).size.height * 0.9,
+        maxHeight: MediaQuery.of(context).size.height * 0.7,
         minWidth: MediaQuery.of(context).size.width * 0.8,
-        minHeight: MediaQuery.of(context).size.height * 0.8,
-        cardBuilder: (context, index) => Container(
-          color: Color.fromRGBO(29, 29, 28, 1.0),
-
-          //color: Color.fromRGBO(29, 29, 28, 1.0),
-          child: Stack(
-            fit: StackFit.expand,
-            children: <Widget>[
-              Column(
-                children: <Widget>[
-                  _datosPersona(personas, index),
-                  Expanded(
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(20),
-                      child: Image(
-                        image: AssetImage(personas[index].photoURL),
-                        fit: BoxFit.fill,
-                      ),
-                    ),
-                  ),
-                ],
-              )
-            ],
+        minHeight: MediaQuery.of(context).size.height * 0.6,
+        cardBuilder: (context, index) => ClipRRect(
+          borderRadius: BorderRadius.circular(20),
+          child: Container(
+            decoration: BoxDecoration(
+              color: Colors.red,
+            ),
+            // Color.fromRGBO(29, 29, 28, 1.0),
+            child: Stack(
+              fit: StackFit.expand,
+              children: <Widget>[
+                Column(
+                  children: <Widget>[
+                    _datosPersona(personas, index),
+                    _foto(personas, index),
+                  ],
+                )
+              ],
+            ),
           ),
         ),
         cardController: controller = CardController(),
@@ -95,25 +108,39 @@ class _SwipePageState extends State<SwipePage> {
     );
   }
 
-  Widget _datosPersona(List<CardInfo> personas, int index) {
-    TextStyle estilo = new TextStyle(fontWeight: FontWeight.bold, color: Colors.white, fontSize: 23.0);
-
-    return SafeArea(
-      child: Container(
-        padding: EdgeInsets.all(20),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Text(
-              '${personas[index].name}, ${personas[index].age}',
-              style: estilo,
-            ),
-            Text(
-              personas[index].country,
-              style: estilo,
-            ),
-          ],
+  Expanded _foto(List<CardInfo> personas, int index) {
+    return Expanded(
+      child: Image(
+        image: AssetImage(
+          personas[index].photoURL,
         ),
+        fit: BoxFit.fill,
+        width: double.infinity,
+      ),
+    );
+  }
+
+  Widget _datosPersona(List<CardInfo> personas, int index) {
+    TextStyle estilo = new TextStyle(
+        fontWeight: FontWeight.bold, color: Colors.white, fontSize: 23.0);
+
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      height: MediaQuery.of(context).size.height * 0.13,
+      color: Colors.black,
+      padding: EdgeInsets.all(20),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text(
+            '${personas[index].name}, ${personas[index].age}',
+            style: estilo,
+          ),
+          Text(
+            personas[index].country,
+            style: estilo,
+          ),
+        ],
       ),
     );
   }
